ff2c72b201bc318b0dcb377de48e896f43712a1c
luisreciomelero <lreciog@gmail.com>
2021-02-04 14:10:44 +0100

    anadido mvn a sh

--- service-verticle/run_cluster_element_1.sh
+++ service-verticle/run_cluster_element_1.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+mvn clean package -DskipTests
 java $JMX_OPTIONS -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar target/service-verticle-fat.jar -cluster prueba-cluster
--- vertx-rest-api-example/run_cluster_element_1.sh
+++ vertx-rest-api-example/run_cluster_element_1.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+mvn clean package -DskipTests
 JMX_OPTIONS="-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=7778 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=paradigma.pruebavertx "
 java $JMX_OPTIONS -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar target/observer-pattern-fat.jar -cluster prueba-cluster
